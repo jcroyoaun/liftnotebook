7f397253d973ed29a3456982e933d458c65ce5a1
Juan Carlos Royo Aun <jcroyoaun@gmail.com>
2025-03-17 22:46:31 -0600

    Update 000001_20250317061906_auto_migration.up.sql

--- db/migrations/000001_20250317061906_auto_migration.up.sql
+++ db/migrations/000001_20250317061906_auto_migration.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS exercises (
     id bigserial PRIMARY KEY,  
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     name text NOT NULL,
-    primarymusclegroups text[] NOT NULL,
-    secondarymusclegroups text[] NOT NULL,
+    primarymuscles text[] NOT NULL,
+    secondarymuscles text[] NOT NULL,
     version integer NOT NULL DEFAULT 1
 );
