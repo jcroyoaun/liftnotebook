43dea3df75b2fae29c8b4235151af4696e01aa5a
Ceviche DB Sync Operator <operator@example.com>
2025-03-19 05:41:44 +0000

    Updated database dump

---
+++ dumps/dump.sql
@@ -0,0 +1,95 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.8
+-- Dumped by pg_dump version 17.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER TABLE IF EXISTS ONLY public.exercises DROP CONSTRAINT IF EXISTS exercises_pkey;
+ALTER TABLE IF EXISTS public.exercises ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE IF EXISTS public.exercises_id_seq;
+DROP TABLE IF EXISTS public.exercises;
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: exercises; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.exercises (
+    id bigint NOT NULL,
+    created_at timestamp(0) with time zone DEFAULT now() NOT NULL,
+    name text NOT NULL,
+    primarymuscles text[] NOT NULL,
+    secondarymuscles text[],
+    version integer DEFAULT 1 NOT NULL
+);
+
+
+--
+-- Name: exercises_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.exercises_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: exercises_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.exercises_id_seq OWNED BY public.exercises.id;
+
+
+--
+-- Name: exercises id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.exercises ALTER COLUMN id SET DEFAULT nextval('public.exercises_id_seq'::regclass);
+
+
+--
+-- Data for Name: exercises; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.exercises (id, created_at, name, primarymuscles, secondarymuscles, version) FROM stdin;
+1	2025-03-19 05:40:56+00	Flat Bench Press	{"Chest, Anterior Delts"}	{Triceps}	1
+\.
+
+
+--
+-- Name: exercises_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.exercises_id_seq', 1, true);
+
+
+--
+-- Name: exercises exercises_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.exercises
+    ADD CONSTRAINT exercises_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
