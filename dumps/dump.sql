04075f8c1140a22e372cb7f1bf5cd33c09d41fe2
Ceviche DB Sync Operator <operator@example.com>
2025-07-12 00:22:22 +0000

    Updated database dump

--- dumps/dump.sql
+++ dumps/dump.sql
@@ -17,10 +17,30 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE IF EXISTS ONLY public.users DROP CONSTRAINT IF EXISTS users_pkey;
+ALTER TABLE IF EXISTS ONLY public.users DROP CONSTRAINT IF EXISTS users_email_key;
 ALTER TABLE IF EXISTS ONLY public.exercises DROP CONSTRAINT IF EXISTS exercises_pkey;
+ALTER TABLE IF EXISTS public.users ALTER COLUMN id DROP DEFAULT;
 ALTER TABLE IF EXISTS public.exercises ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE IF EXISTS public.users_id_seq;
+DROP TABLE IF EXISTS public.users;
 DROP SEQUENCE IF EXISTS public.exercises_id_seq;
 DROP TABLE IF EXISTS public.exercises;
+DROP EXTENSION IF EXISTS citext;
+--
+-- Name: citext; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings';
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -36,16 +56,6 @@ CREATE TABLE public.exercises (
     primarymuscles text[] NOT NULL,
     secondarymuscles text[],
     version integer DEFAULT 1 NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS public.users (
-    id bigserial PRIMARY KEY,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    name text NOT NULL,
-    email citext UNIQUE NOT NULL,
-    password_hash bytea NOT NULL,
-    activated bool NOT NULL,
-    version integer NOT NULL DEFAULT 1
 );
 
 
@@ -69,6 +79,40 @@ ALTER SEQUENCE public.exercises_id_seq OWNED BY public.exercises.id;
 
 
 --
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    created_at timestamp(0) with time zone DEFAULT now() NOT NULL,
+    name text NOT NULL,
+    email public.citext NOT NULL,
+    password_hash bytea NOT NULL,
+    activated boolean NOT NULL,
+    version integer DEFAULT 1 NOT NULL
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
 -- Name: exercises id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -76,13 +120,28 @@ ALTER TABLE ONLY public.exercises ALTER COLUMN id SET DEFAULT nextval('public.ex
 
 
 --
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
 -- Data for Name: exercises; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 COPY public.exercises (id, created_at, name, primarymuscles, secondarymuscles, version) FROM stdin;
-1	2025-03-19 05:40:56+00	Flat Bench Press	{"Chest", "Anterior Delts"}	{Triceps}	1
+1	2025-03-19 05:40:56+00	Flat Bench Press	{Chest,"Anterior Delts"}	{Triceps}	1
 2	2025-03-19 05:48:09+00	Hack Squat	{Quadriceps}	{Glutes}	1
-3	2025-03-19 06:37:19+00	Romanian Deadlift	{"Hamstrings", "Glutes"}	{"Erectors", "Lats"}	2
+3	2025-03-19 06:37:19+00	Romanian Deadlift	{Hamstrings,Glutes}	{Erectors,Lats}	2
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.users (id, created_at, name, email, password_hash, activated, version) FROM stdin;
 \.
 
 
@@ -94,11 +153,34 @@ SELECT pg_catalog.setval('public.exercises_id_seq', 3, true);
 
 
 --
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
 -- Name: exercises exercises_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.exercises
     ADD CONSTRAINT exercises_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
 --
